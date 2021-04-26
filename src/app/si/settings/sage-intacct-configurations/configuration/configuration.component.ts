abe8d7b4a0d77b3f854f7d61e58824d1c4ea5c07
Nilesh Pant <nileshpant6969@gmail.com>
2021-04-26 22:07:01 +0530

    minor change

--- src/app/si/settings/sage-intacct-configurations/configuration/configuration.component.ts
+++ src/app/si/settings/sage-intacct-configurations/configuration/configuration.component.ts
@@ -226,7 +226,9 @@ export class ConfigurationComponent implements OnInit {
             that.snackBar.open('Auto mapping of employees may take up to 10 minutes');
           }, 1500);
         }
-        
+
+        that.router.navigateByUrl(`workspaces/${that.workspaceId}/dashboard`);
+
         that.isLoading = false;
       });
     } else {
