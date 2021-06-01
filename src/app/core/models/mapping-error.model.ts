f0dbab3f991418b80c0ad8ae8e00bc145859e3f2
Nilesh Pant <nileshpant6969@gmail.com>
2021-06-01 10:56:31 +0530

    remove redirect url id from mapping error model

--- src/app/core/models/mapping-error.model.ts
+++ src/app/core/models/mapping-error.model.ts
@@ -1,7 +1,6 @@
 /* tslint:disable */
 // TODO: Use something for serialization / deserialization
 export type MappingError = {
-  redirected_url_id: string;
   expense_group_id: number;
   message: string;
   row: number;
