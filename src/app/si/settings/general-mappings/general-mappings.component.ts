6db80fabd28da73b463f4d093920e78857bc1c49
labhvam5 <labhvamkrsharma@gmail.com>
2021-08-05 12:51:26 +0530

    updated package-lock.json and component.ts file

--- src/app/si/settings/general-mappings/general-mappings.component.ts
+++ src/app/si/settings/general-mappings/general-mappings.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MappingsService } from '../../../core/services/mappings.service';
