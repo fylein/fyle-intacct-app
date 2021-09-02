687c811af7b43bd3e5c6f4b374b541f1b591d049
labhvam5 <88420539+labhvam5@users.noreply.github.com>
2021-09-02 13:03:44 +0530

    sentry integration (#64)
    * sentry integration

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,5 +5,7 @@ export const environment = {
     callback_uri: $ENV.CALLBACK_URI,
     api_url: $ENV.API_URL,
     app_url: $ENV.APP_URL,
-    hotjar_id: $ENV.HOTJAR_ID
+    hotjar_id: $ENV.HOTJAR_ID,
+    sentry_dsn: $ENV.SENTRY_DSN,
+    release: $ENV.RELEASE
 };
