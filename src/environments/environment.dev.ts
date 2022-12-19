f916f2cca3536852512c1b2794931ee7236c4b17
Vishal Pandey <vishalpandeyviptsk@gmail.com>
2022-12-19 14:17:14 +0530

    simplify report closure

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
     fyle_url: $ENV.FYLE_URL,
+    fyle_app_url: $ENV.FYLE_URL,
     fyle_client_id: $ENV.FYLE_CLIENT_ID,
     callback_uri: $ENV.CALLBACK_URI,
     api_url: $ENV.API_URL,
