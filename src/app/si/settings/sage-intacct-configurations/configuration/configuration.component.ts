7fc0033776b7168801ccf066d5438ce022892e10
Fyle <fyle@Fyles-MacBook-Air.local>
2023-04-17 12:41:23 +0530

    Card to Card mapping App changes lint fix

--- src/app/si/settings/sage-intacct-configurations/configuration/configuration.component.ts
+++ src/app/si/settings/sage-intacct-configurations/configuration/configuration.component.ts
@@ -406,7 +406,7 @@ export class ConfigurationComponent implements OnInit {
 
     const importProjects = that.configurationForm.value.importProjects ? that.configurationForm.value.importProjects : false;
     const importTaxCodes = that.configurationForm.value.importTaxCodes ? that.configurationForm.value.importTaxCodes : false;
-    const chargeCard = that.configurationForm.value.cccExpense === 'CHARGE_CARD_TRANSACTION' ? true : false; 
+    const chargeCard = that.configurationForm.value.cccExpense === 'CHARGE_CARD_TRANSACTION' ? true : false;
 
     if (importTaxCodes) {
       mappingsSettingsPayload.push({
@@ -440,11 +440,11 @@ export class ConfigurationComponent implements OnInit {
       }
     }
 
-    if(chargeCard) {
+    if (chargeCard) {
       mappingsSettingsPayload.push({
         source_field: 'CORPORATE_CARD',
-        destination_field: 'CHARGE_CARD_NUMBER'  
-      })
+        destination_field: 'CHARGE_CARD_NUMBER'
+      });
     }
     return mappingsSettingsPayload;
   }
