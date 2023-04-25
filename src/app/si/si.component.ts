04177b8e266d425da4a857590913ca3fa9973cae
Fyle <fyle@Fyles-MacBook-Air.local>
2023-04-25 15:31:47 +0530

    hinding corporate cards

--- src/app/si/si.component.ts
+++ src/app/si/si.component.ts
@@ -61,13 +61,10 @@ export class SiComponent implements OnInit {
   refreshDashboardMappingSettings(mappingSettings: MappingSetting[]) {
     const that = this;
 
-    that.getConfigurations().subscribe((results) => {
-      this.configuration = results[0];
-      that.mappingSettings = mappingSettings.filter(
-        setting => (setting.source_field !== 'EMPLOYEE' && setting.source_field !== 'CATEGORY')
-      );
-      that.isLoading = false;
-    });
+    that.mappingSettings = mappingSettings.filter(
+      setting => (setting.source_field !== 'EMPLOYEE' && setting.source_field !== 'CATEGORY')
+    );
+    that.isLoading = false;
   }
 
 
