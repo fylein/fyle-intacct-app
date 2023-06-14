14b4d31a814b2ea99dc52c1cd6b4c31d7f00d035
ashwin1111 <winash1998@gmail.com>
2023-06-14 16:54:38 +0530

    Refactor Import Dependent Fields

---
+++ src/app/core/models/dependent-fields.model.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+
+export interface DependentField extends DependentFieldPost {
+    id: number;
+    project_field_id: number;
+    cost_code_field_id: number;
+    cost_type_field_id: number;
+    last_successful_import_at: string;
+    created_at: string;
+    updated_at: string;    
+};
+
+export type DependentFieldPost = {
+    is_import_enabled: boolean;
+    cost_code_field_name: string;
+    cost_type_field_name: string;
+    workspace: number;
+}
